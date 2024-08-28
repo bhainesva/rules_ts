ada385d07919c24c52dbb05a1593d2118fe62d70
Ben McGinnis <bmcginnis@yext.com>
2024-08-28 12:25:53 -0400

    depend on one of the other examples as a poc

--- examples/mixed_sources/main.ts
+++ examples/mixed_sources/main.ts
@@ -1,9 +1,9 @@
 import { lib1 } from "./lib/lib1"
-import { lib2 } from "./lib/lib2"
+import js_lib from "../js_lib/index"
 
 const main = () => {
     lib1();
-    lib2();
+    console.log(js_lib);
 }
 
 main();
